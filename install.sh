47ac8c147945ccb31abdf2419f09fc91a5e998ff
Washington Botelho <wbotelhos@gmail.com>
2012-10-12 14:28:01 -0300

    Install no loger call macosx.sh.

--- install.sh
+++ install.sh
@@ -1,26 +1,43 @@
-#! /bin/bash
-GREEN="\033[0;32m"
-NO_COLOR="\033[1;0m"
-GRAY="\033[0;36m"
+#!/bin/bash
+GREEN='\033[0;32m'
+NO_COLOR='\033[1;0m'
+GRAY='\033[0;36m'
+RED='\033[1;31m'
 
-echo -e "\n${GREEN}Initializing...${NO_COLOR}\n"
+echo -e "\n${GRAY}Copying config files...${NO_COLOR}\n"
 
+echo '.bash_profile'
 cp files/bash_profile ~/.bash_profile
+
+echo '.caprc'
 cp files/caprc ~/.caprc
+
+echo '.gemrc'
 cp files/gemrc ~/.gemrc
+
+echo '.gitconfig'
 cp files/gitconfig ~/.gitconfig
+
+echo '.gitignore'
 cp files/gitignore ~/.gitignore
+
+echo '.hgrc'
 cp files/hgrc ~/.hgrc
+
+echo '.irbrc'
 cp files/irbrc ~/.irbrc
+
+echo '.pryrc'
 cp files/pryrc ~/.pryrc
-#cp files/rspec ~/.rspec
 
-./macosx.sh
+echo '.rspec'
+cp files/rspec ~/.rspec
 
+echo -e "\n${GRAY}Refreshing the .bash_profile${NO_COLOR}"
 source ~/.bash_profile
 
-echo -e "${GRAY}\n* Copy the rspec file as .rspec to your projects!"
-echo -e "* Copy the gitignore file as .gitignore to your projects!"
+echo -e "\n${GRAY}* Copy the gitignore file as .gitignore to your projects!"
 echo -e "\n${GREEN}Done!${NO_COLOR}\n"
 
-open files/Professional.terminal
+echo 'Applying Terminal config...'
+open more/Professional.terminal
