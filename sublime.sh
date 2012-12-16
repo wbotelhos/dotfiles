98f4136f63963f3ddfedd3257244b5176cac59d9
Washington Botelho <wbotelhos@gmail.com>
2012-12-15 23:13:33 -0200

    Added Sublime snippets.

--- sublime.sh
+++ sublime.sh
@@ -11,18 +11,21 @@ LIBRARY_PATH=/Users/$USER/Library
 SUBLIME_APP=$(ls /Applications/Sublime\ Text\ 2.app 2> /dev/null)
 
 if [ $SUBLIME_APP ]; then
-	echo -e "\n${GRAY}Generating the alias '${ALIAS}' open command...${NO_COLOR}"
+  echo -e "\n${GRAY}Generating the alias '${ALIAS}' open command...${NO_COLOR}"
 
-	if [ -f $ALIAS_PATH ]; then
-		echo -e "${RED}The alias '${ALIAS}' already exists! [skipped]${NO_COLOR}"
-	else
-		sudo ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl $ALIAS_PATH
-	fi
+  if [ -f $ALIAS_PATH ]; then
+    echo -e "${RED}The alias '${ALIAS}' already exists! [skipped]${NO_COLOR}"
+  else
+    sudo ln -s /Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl $ALIAS_PATH
+  fi
 
-	echo -e "\n${GRAY}Copying user profile file...${NO_COLOR}"
-	cp ./more/Preferences.sublime-settings $LIBRARY_PATH/Application\ Support/Sublime\ Text\ 2/Packages/User
+  echo -e "\n${GRAY}Copying user profile file...${NO_COLOR}"
+  cp ./sublime/Preferences.sublime-settings $LIBRARY_PATH/Application\ Support/Sublime\ Text\ 2/Packages/User
+
+  echo -e "\n${GRAY}Copying snippet files...${NO_COLOR}"
+  cp ./sublime/*.sublime-snippet $LIBRARY_PATH/Application\ Support/Sublime\ Text\ 2/Packages/User
 else
-	echo "The 'Sublime Text 2.app' is not installed yet. Do it first!"
+  echo "The 'Sublime Text 2.app' is not installed yet. Do it first!"
 fi
 
 echo -e "\n${GREEN}Done!${NO_COLOR}\n"
