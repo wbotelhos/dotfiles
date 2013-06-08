c9f8897d3c93fbbc4866b1776491148a4b76beb2
Washington Botelho <wbotelhos@gmail.com>
2013-06-08 00:15:45 -0300

    Updating plugins.

--- sublime/plugin.sh
+++ sublime/plugin.sh
@@ -34,18 +34,19 @@ log_backup_and_install() {
   echo -e "\n${GRAY}Backuping $1 folder and install a new one...${NO_COLOR}"
 }
 
-download 'https://github.com/colinta/SublimeChangeQuotes'             'ChangeQuotes'
-download 'https://github.com/eddorre/SublimeERB'                      'ERB'
-download 'https://github.com/jnordberg/sublime-colorpick'             'ColorPicker'
-download 'https://github.com/kek/sublime-expand-selection-to-quotes'  'ExpandQuotes'
-download 'https://github.com/kemayo/sublime-text-2-git'               'Git'
-download 'https://github.com/Kronuz/SublimeCodeIntel'                 'CodeIntel'
-download 'https://github.com/phillipkoebbe/DetectSyntax'              'DetectSyntax'
-download 'https://github.com/sergeche/emmet-sublime'                  'Emmet'
-download 'https://github.com/SublimeText/RSpec'                       'RSpec'
-download 'https://github.com/titoBouzout/SideBarEnhancements'         'SideBarEnhancements'
-download 'https://github.com/wbond/sublime_alignment'                 'Alignment'
-download 'https://github.com/wbotelhos/sublime-assignment'            'Assignment'
-download 'https://github.com/zoomix/SublimeToggleSymbol'              'ToggleSymbol'
+download 'https://github.com/colinta/SublimeChangeQuotes'            'ChangeQuotes'
+download 'https://github.com/eddorre/SublimeERB'                     'ERB'
+download 'https://github.com/jnordberg/sublime-colorpick'            'ColorPicker'
+download 'https://github.com/kek/sublime-expand-selection-to-quotes' 'ExpandQuotes'
+download 'https://github.com/kemayo/sublime-text-2-git'              'Git'
+download 'https://github.com/Kronuz/SublimeCodeIntel'                'CodeIntel'
+download 'https://github.com/facelessuser/ApplySyntax'               'ApplySyntax'
+download 'https://github.com/sergeche/emmet-sublime'                 'Emmet'
+download 'https://github.com/SublimeText/RSpec'                      'RSpec'
+download 'https://github.com/titoBouzout/SideBarEnhancements'        'SideBarEnhancements'
+download 'https://github.com/wbond/sublime_alignment'                'Alignment'
+download 'https://github.com/wbotelhos/sublime-assignment'           'Assignment'
+download 'https://github.com/zoomix/SublimeToggleSymbol'             'ToggleSymbol'
+download 'https://github.com/adampresley/sublime-js-minify'          'JsMinify'
 
 echo -e "\n${GREEN}plugin.sh done!${NO_COLOR}\n"
