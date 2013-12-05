49507f8685d6dbcaace462f4f02042addedef66b
Washington Botelho <wbotelhos@gmail.com>
2013-12-05 17:30:20 -0200

    add: fluxbox files.

---
+++ fluxbox.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+GRAY='\033[0;36m'
+GREEN='\033[0;32m'
+NO_COLOR='\033[1;0m'
+
+LOCALE="${HOME}/.fluxbox"
+
+echo -e "\n${GREEN}Updating the repository...${NO_COLOR}\n"
+sudo apt-get update
+
+echo -e "\n${GREEN}Installing Fluxbox...${NO_COLOR}\n"
+sudo apt-get install fluxbox
+
+rm -rf $LOCALE
+
+echo -e "\n${GREEN}Copying Fluxbox files...${NO_COLOR}\n"
+cp -r ./fluxbox $LOCALE
+
+echo -e "\n${GREEN}Done!${NO_COLOR}\n"
