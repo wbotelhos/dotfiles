0ca3aa58d4896c02a794fe2be62f16e894e2ff8d
Washington Botelho <wbotelhos@gmail.com>
2013-12-05 17:30:00 -0200

    update: letting plugin install on the final.

--- sublime.sh
+++ sublime.sh
@@ -13,10 +13,10 @@ if [ -e "$path" ]; then
   cd sublime
   ./alias.sh
   ./keymap.sh
-  ./plugin.sh
   ./settings.sh
   ./snippet.sh
   ./syntax.sh
+  ./plugin.sh
 else
   echo "The 'Sublime Text 2' is not installed yet. Do it first!"
 fi
