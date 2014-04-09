41b24e2b43a0969ea3ba8be478a92031a6108a61
Washington Botelho <wbotelhos@gmail.com>
2014-04-09 19:22:46 -0300

    update: always removing the alias to make sure the right one.

--- sublime/alias.sh
+++ sublime/alias.sh
@@ -4,19 +4,19 @@ GREEN='\033[0;32m'
 NO_COLOR='\033[1;0m'
 RED='\033[1;31m'
 
-aliaz='sublime'
-alias_path=/usr/local/bin/${aliaz}
+ALIAS_NAME='sublime'
+ALIAS_PATH=/usr/local/bin/${ALIAS_NAME}
 
-echo -e "\n${GRAY}Generating the alias '${aliaz}' open command...${NO_COLOR}"
+echo -e "\n${GRAY}Generating the alias '${ALIAS_NAME}' open command...${NO_COLOR}"
 
-if [ -L "$alias_path" ]; then
-  echo -e "${RED}The alias '${aliaz}' already exists! [skipped]${NO_COLOR}"
+if [ -L "$ALIAS_PATH" ]; then
+  sudo unlink $ALIAS_PATH
+fi
+
+if [ `uname` == 'Linux' ]; then
+  sudo ln -s "/home/${USER}/Development/Sublime Text 2/sublime_text" $ALIAS_PATH
 else
-  if [ `uname` == 'Linux' ]; then
-    sudo ln -s '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl' $alias_path
-  else
-    sudo ln -s "/home/${USER}/Development/Sublime Text 2/sublime_text" $alias_path
-  fi
+  sudo ln -s '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl' $ALIAS_PATH
 fi
 
 echo -e "${GREEN}alias.sh done!${NO_COLOR}"
